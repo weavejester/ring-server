f03fd28d611bfaa025a371097e773e05540f3455
James Reeves <jreeves@weavejester.com>
2012-03-11 21:15:14 +0000

    Dev dependencies changed to Lein 2 format

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/core.incubator "0.1.0"]
                  [ring "1.0.2"]]
-  :dev-dependencies [[clj-http "0.3.1"]
-                     [codox "0.4.0"]])
+  :profiles {:dev {:dependencies [[clj-http "0.3.1"]
+                                  [codox "0.4.0"]]}})

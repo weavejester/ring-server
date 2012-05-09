790070fbc6c4138847f2441e37ce9f4972346a2b
James Reeves <jreeves@weavejester.com>
2012-05-09 21:54:27 +0100

    Updated Codox to 0.6.1, made plugin dependency

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/core.incubator "0.1.0"]
                  [ring "1.0.2"]]
-  :profiles {:dev {:dependencies [[clj-http "0.3.1"]
-                                  [codox "0.4.0"]]}})
+  :plugins [[codox "0.6.1"]]
+  :profiles {:dev {:dependencies [[clj-http "0.3.1"]]}})

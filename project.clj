e6dcea69c9cfd129982a5d52f8a4af038943e9bf
James Reeves <jreeves@weavejester.com>
2013-01-19 20:17:57 +0000

    Updated Ring version to 1.1.7

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Library for running Ring web servers"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/core.incubator "0.1.0"]
-                 [ring "1.1.4"]
+                 [ring "1.1.7"]
                  [ring-refresh "0.1.1"]]
   :plugins [[codox "0.6.1"]]
   :profiles {:dev {:dependencies [[clj-http "0.4.1"]]}})

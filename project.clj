8d4718af062fc5884400d35a9b798453de31ae31
James Reeves <jreeves@weavejester.com>
2013-01-20 17:09:46 +0000

    Released 0.2.7

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ring-server "0.2.6"
+(defproject ring-server "0.2.7"
   :description "Library for running Ring web servers"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/core.incubator "0.1.0"]

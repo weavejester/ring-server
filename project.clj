6b70ee4b12672cdc9be2768413f78714fefe0590
James Reeves <jreeves@weavejester.com>
2013-01-20 16:23:06 +0000

    Updated ring-refresh dependency

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/core.incubator "0.1.0"]
                  [ring "1.1.7"]
-                 [ring-refresh "0.1.1"]]
+                 [ring-refresh "0.1.2"]]
   :plugins [[codox "0.6.1"]]
   :profiles {:dev {:dependencies [[clj-http "0.4.1"]]}})
